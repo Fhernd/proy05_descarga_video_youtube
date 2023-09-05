f30a965fc747a0378a096e76a23672160bf51f5b
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-05 15:45:33 -0500

    Primer ejemplo de descarga de un vídeo desde YouTube

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Proyecto 5: Descarga Vídeo YouTube')
+from pytube import YouTube
+
+# URL del video que deseas descargar
+video_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+# Crear objeto YouTube
+yt = YouTube(video_url)
+
+# Obtener el stream de video de mayor resolución
+video_stream = yt.streams.get_highest_resolution()
+
+# Descargar el video en el directorio actual
+video_stream.download()
+
+print(f"Video '{yt.title}' descargado exitosamente!")
